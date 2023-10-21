c5d3377a6b409b8efea6ba85a58ad4221ac7345c
Ayooluwa Toluwanimi <ijidakinroayooluwa@gmail.com>
2023-10-21 14:04:26 -0700

    Add files via upload

---
+++ Bro-Wale-website/script.js
@@ -0,0 +1,21 @@
+const text = ["I am a Graphic designer", "I am an Interior Decorator", "I am an Event planner"];
+let currentText = 0;
+let letter = 0;
+let typingDisplay = document.getElementById("typingText");
+
+(function type() {
+    // Check if current text has completed typing
+    if (letter < text[currentText].length) {
+        typingDisplay.textContent += text[currentText].charAt(letter);
+        letter++;
+        setTimeout(type, 200);  // Determines the speed of typing
+    } else {
+        // Pause before starting a new sentence
+        setTimeout(() => {
+            typingDisplay.textContent = "";
+            currentText = (currentText + 1) % text.length;
+            letter = 0;
+            type();
+        }, 2000);  // Determines the pause duration between texts
+    }
+})();
